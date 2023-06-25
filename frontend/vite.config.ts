b52d29851c3581ca05ab2da4110116ca62288d3c
ajkarell <111962031+ajkarell@users.noreply.github.com>
2023-06-25 16:17:06 +0300

    frontend: add PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,52 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Ordo',
+        short_name: 'Ordo',
+        description: 'Keikkatyön hallintapalvelu',
+        lang: 'fi',
+        theme_color: '#ffffff',
+        start_url: '/',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:7032',
